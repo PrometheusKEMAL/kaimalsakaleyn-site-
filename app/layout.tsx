7f71be0610465bfd3230245cc5410a274a4346b3
Kemal <kemal@example.com>
2026-08-31 14:15:55 +0300

    Revert music player feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter, Cinzel, Noto_Naskh_Arabic } from "next/font/google";
 import { siteConfig } from "@/config/site";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
-import { MusicPlayer } from "@/components/layout/MusicPlayer";
 import "./globals.css";
 
 const inter = Inter({
@@ -73,9 +72,6 @@ export default function RootLayout({
 
         {/* Footer */}
         <Footer />
-        
-        {/* Background Music */}
-        <MusicPlayer />
       </body>
     </html>
   );
